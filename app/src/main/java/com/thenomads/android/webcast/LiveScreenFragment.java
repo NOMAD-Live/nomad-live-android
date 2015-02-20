41cc40970d36d321171b92206fcf0dc4c0ed746c
C-A de Salaberry <cadesalaberry@yahoo.com>
2015-02-20 14:21:16 -0500

    Mod: Better server error management.

--- app/src/main/java/com/thenomads/android/webcast/LiveScreenFragment.java
+++ app/src/main/java/com/thenomads/android/webcast/LiveScreenFragment.java
@@ -19,12 +19,22 @@ public class LiveScreenFragment extends Fragment {
         final Button button = (Button) rootView.findViewById(R.id.dummy_button);
 
         //final String videoPath = getString(R.string.wowza_vod_hls);
-        final String videoPath = getString(R.raw.dancefloor);
-
+        final String videoPath = "nothing";
         button.setText(videoPath);
 
-        // Retrieve video from Wowza stream
-        myVideoView.setVideoPath(videoPath);
+        if (Toolbox.isWowzaAvailable(videoPath)) {
+
+            // Retrieve video from Wowza stream
+            myVideoView.setVideoPath(videoPath);
+            button.setText("Wowza seems OK");
+
+        } else {
+
+            String path = "android.resource://" + rootView.getContext().getPackageName() + "/" + R.raw.dancefloor;
+
+            myVideoView.setVideoPath(path);
+            button.setText("Server Unavailable");
+        }
 
         return rootView;
     }
---
+++ app/src/main/java/com/thenomads/android/webcast/Toolbox.java
@@ -0,0 +1,65 @@
+package com.thenomads.android.webcast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * A small toolbox with small method useful for the project.
+ */
+class Toolbox {
+
+    /**
+     * If an instance of Wowza is running at the given url, it should return something like:
+     * <p/>
+     * Wowza Streaming Engine 4 Trial Edition (Expires: Aug 10, 2015) 4.1.1 build13180
+     *
+     * @param unsafeURL a url to test
+     * @return returns true if a Wowza instance seems to be running
+     */
+    public static boolean isWowzaAvailable(String unsafeURL) {
+
+        boolean available = false;
+
+        try {
+            URL url = new URL(unsafeURL);
+
+            // Strips to just protocol + host + port
+            URL simpleURL = new URL(url.getProtocol() + "://" + url.getAuthority());
+
+            // Retrieves content
+            String content = Toolbox.getURLContentAsString(simpleURL);
+
+            if (content.contains("Wowza Streaming Engine")) {
+                available = true;
+            }
+
+
+        } catch (IOException e) {
+            // Fails silently.
+            // If a local path is passed it will do nothing.
+        }
+
+        return available;
+
+    }
+
+    private static String getURLContentAsString(URL url) throws IOException {
+
+        URLConnection urlConnection = url.openConnection();
+        InputStream is = urlConnection.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+
+        int numCharsRead;
+        char[] charArray = new char[1024];
+        StringBuilder sb = new StringBuilder();
+
+        while ((numCharsRead = isr.read(charArray)) > 0) {
+            sb.append(charArray, 0, numCharsRead);
+        }
+
+        return sb.toString();
+    }
+}
