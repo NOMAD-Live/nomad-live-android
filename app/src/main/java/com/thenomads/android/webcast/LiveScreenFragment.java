0dd979ea2e54c5f2fa088cd2c48c62fd0b19f9f4
C-A de Salaberry <cadesalaberry@yahoo.com>
2015-02-23 16:26:35 -0500

    Mod: Turn button into toggle.

--- app/src/main/java/com/thenomads/android/webcast/LiveScreenFragment.java
+++ app/src/main/java/com/thenomads/android/webcast/LiveScreenFragment.java
@@ -6,14 +6,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
-import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.VideoView;
 
 public class LiveScreenFragment extends Fragment {
 
     private View mRootView;
-    private Button mButton;
+    private Switch mSwitch;
     private String mVideoPath;
+    private String mLocalPath;
     private WebView mTwitterBannerWebView;
     private VideoView mLiveVideoView;
 
@@ -23,18 +25,32 @@ public class LiveScreenFragment extends Fragment {
 
         mRootView = inflater.inflate(R.layout.fragment_fullscreen_video, container, false);
         mLiveVideoView = (VideoView) mRootView.findViewById(R.id.fullscreen_content);
-        mButton = (Button) mRootView.findViewById(R.id.playback_button);
+//        mButton = (Button) mRootView.findViewById(R.id.playback_button);
+        mSwitch = (Switch) mRootView.findViewById(R.id.offline_switch);
 
         mVideoPath = getString(R.string.wowza_vod_hls);
+        mLocalPath = "android.resource://" + mRootView.getContext().getPackageName() + "/" + R.raw.dancefloor;
 
-        // Takes care of the video side
-        mButton.setText("Playing from Wowza.");
-        mLiveVideoView.setVideoPath(mVideoPath);
 
-        mButton.setOnClickListener(new View.OnClickListener() {
+        // Takes care of the video side, defaults to offline
+        mLiveVideoView.setVideoPath(mLocalPath);
+        mSwitch.setChecked(false);
+
+        mSwitch.setTextOff("Offline");
+        mSwitch.setTextOn("Online");
+
+        mSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
             @Override
-            public void onClick(View v) {
-                togglePlayback();
+            public void onCheckedChanged(CompoundButton buttonView,
+                                         boolean isChecked) {
+
+                if (isChecked) {
+                    mLiveVideoView.setVideoPath(mVideoPath);
+                } else {
+                    mLiveVideoView.setVideoPath(mLocalPath);
+                }
+
             }
         });
 
@@ -43,23 +59,6 @@ public class LiveScreenFragment extends Fragment {
         mTwitterBannerWebView = (WebView) mRootView.findViewById(R.id.twitter_banner);
         mTwitterBannerWebView.loadUrl("file:///android_asset/banner.html");
 
-
         return mRootView;
-    }
-
-    public void togglePlayback() {
-        if (mButton.getText().toString().startsWith("Playing Local")) {
-
-            // Retrieve video from Wowza stream
-            mLiveVideoView.setVideoPath(mVideoPath);
-            mButton.setText("Playing from Wowza.");
-
-        } else {
-
-            String path = "android.resource://" + mRootView.getContext().getPackageName() + "/" + R.raw.dancefloor;
-
-            mLiveVideoView.setVideoPath(path);
-            mButton.setText("Playing Local File.");
-        }
     }
 }
