116a74f4b95204149cc090b8b94bc2d8f70f014c
C-A de Salaberry <cadesalaberry@yahoo.com>
2015-03-11 12:02:19 -0400

    Mod: Disable pause option.

--- app/src/main/java/com/thenomads/android/nomadlive/video/IntuitiveFullScreenVideoView.java
+++ app/src/main/java/com/thenomads/android/nomadlive/video/IntuitiveFullScreenVideoView.java
@@ -3,12 +3,7 @@ package com.thenomads.android.nomadlive.video;
 import android.content.Context;
 import android.media.MediaPlayer;
 import android.util.AttributeSet;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.Toast;
 import android.widget.VideoView;
-
-import java.util.Calendar;
 
 /**
  * A wrapper around the VideoView to enable smooth control of the player.
@@ -17,9 +12,6 @@ import java.util.Calendar;
  */
 public class IntuitiveFullScreenVideoView extends VideoView {
 
-    private static final int MAX_CLICK_DURATION = 200;
-    private long startClickTime;
-
     public IntuitiveFullScreenVideoView(Context context, AttributeSet attrs) {
         super(context, attrs);
         this.initCustomFullScreenVideoView();
@@ -27,7 +19,6 @@ public class IntuitiveFullScreenVideoView extends VideoView {
 
     private void initCustomFullScreenVideoView() {
         this.loopVideo();
-        this.clickToPause();
     }
 
     private void loopVideo() {
@@ -38,43 +29,4 @@ public class IntuitiveFullScreenVideoView extends VideoView {
             }
         });
     }
-
-    private void clickToPause() {
-
-        this.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent me) {
-
-                switch (me.getAction()) {
-                    case MotionEvent.ACTION_DOWN: {
-                        startClickTime = Calendar.getInstance().getTimeInMillis();
-                        break;
-                    }
-                    case MotionEvent.ACTION_UP: {
-                        // Checks for single tap based on timing
-                        long clickDuration = Calendar.getInstance().getTimeInMillis() - startClickTime;
-
-                        if (clickDuration < MAX_CLICK_DURATION) {
-                            if (isPlaying()) {
-                                pause();
-                                toast("Paused");
-                                return false;
-                            } else {
-                                resume();
-                                start();
-                                toast("Resuming...");
-
-                            }
-                        }
-                    }
-                }
-                return true;
-            }
-        });
-    }
-
-    private void toast(String s) {
-        Toast.makeText(this.getContext(), s, Toast.LENGTH_SHORT).show();
-    }
-
 }
