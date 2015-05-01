3c65a6c1f9ac24cd01de4e250309aeb9ee9cb7c1
C-A de Salaberry <cadesalaberry@yahoo.com>
2015-05-01 18:35:07 -0400

    Fix: Closes #13. Settings button now available on screen.

--- app/src/main/java/com/thenomads/android/nomadlive/SecondFragment.java
+++ app/src/main/java/com/thenomads/android/nomadlive/SecondFragment.java
@@ -1,11 +1,13 @@
 package com.thenomads.android.nomadlive;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.widget.ImageView;
 
 public class SecondFragment extends Fragment {
 
@@ -19,6 +21,17 @@ public class SecondFragment extends Fragment {
         WebView mAboutPage = (WebView) mRootView.findViewById(R.id.about_page);
 
         mAboutPage.loadUrl(getString(R.string.about_page_file));
+
+        // Deals with the settings button
+        ImageView mImageView = (ImageView) mRootView.findViewById(R.id.settings_button);
+        View.OnClickListener mStartBroadcastListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity().getApplicationContext(), SettingsActivity.class);
+                startActivity(i);
+            }
+        };
+        mImageView.setOnClickListener(mStartBroadcastListener);
 
         return mRootView;
     }
