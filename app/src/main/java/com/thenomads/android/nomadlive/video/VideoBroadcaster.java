ea054bc0d7cbe01ed6fedcefb787c1ee0e1f1cd4
C-A de Salaberry <cadesalaberry@yahoo.com>
2015-06-26 13:09:19 -0400

    Add: Stream is created on every new connection !!!

--- app/src/main/java/com/thenomads/android/nomadlive/video/VideoBroadcaster.java
+++ app/src/main/java/com/thenomads/android/nomadlive/video/VideoBroadcaster.java
@@ -1,6 +1,7 @@
 package com.thenomads.android.nomadlive.video;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +11,10 @@ import com.thenomads.android.nomadlive.SECRETS;
 import io.cine.android.BroadcastConfig;
 import io.cine.android.CineIoClient;
 import io.cine.android.CineIoConfig;
+import io.swagger.client.ApiException;
+import io.swagger.client.api.HeartbeatApi;
+import io.swagger.client.api.StreamsApi;
+import io.swagger.client.model.Stream;
 
 /**
  * A wrapper to the Cine.IO Broadcaster.
@@ -17,16 +22,24 @@ import io.cine.android.CineIoConfig;
  */
 public class VideoBroadcaster {
 
-    private static final String TAG = "TwitterTicker";
+    private static final String TAG = "VideoBroadcaster";
 
     private BroadcastConfig mBroadcastConfig;
     private CineIoClient mCineIoClient;
     private Button mBroadcastButton;
     private Context mContext;
 
+    private HeartbeatApi heartbeatApi;
+    private StreamsApi streamsApi;
+    private Stream currentStream;
+    private boolean isStreaming = false;
+
     public VideoBroadcaster(Button b, Context c) {
         this.mBroadcastButton = b;
         this.mContext = c;
+
+        this.streamsApi = new StreamsApi();
+
         startBroadcastActivityOnClick();
     }
 
@@ -52,15 +65,110 @@ public class VideoBroadcaster {
             @Override
             public void onClick(View v) {
 
-                String streamId = "554cf071fc71760b00a78aad";
+                // Uses the current stream or get a new one
+                if (currentStream == null) {
+                    new AsyncTask<Void, Void, Stream>() {
+                        protected Stream doInBackground(Void... params) {
+                            try {
+                                return streamsApi.streamsPost();
 
-                Log.i(TAG, "Starting broadcast on stream" + streamId);
+                            } catch (ApiException e) {
+                                showAPIError("" + e.getCode(), e.getMessage());
+                                e.printStackTrace();
+                            }
+                            return null;
+                        }
 
-                mCineIoClient.broadcast(streamId, mBroadcastConfig, mContext);
+                        @Override
+                        public void onPostExecute(Stream result) {
+
+                            super.onPostExecute(result);
+
+                            if (result != null)
+                                broadcast(result);
+                            else
+                                Log.e(TAG, "Unable to get a new stream.");
+                        }
+                    }.execute();
+                } else {
+                    broadcast(currentStream);
+                }
+
+
+
             }
         };
 
         mBroadcastButton.setOnClickListener(mStartBroadcastListener);
+    }
+
+    public void setStreamingState(boolean isStreaming) {
+        this.isStreaming = isStreaming;
+    }
+
+    public boolean isStreaming() {
+        return this.isStreaming;
+    }
+
+    private void broadcast(Stream stream) {
+
+        this.currentStream = stream;
+
+        if (stream != null && !this.isStreaming()) {
+
+            Log.i(TAG, "Starting broadcast on stream " + stream);
+            Log.i(TAG, "Starting broadcast on stream " + stream.getId());
+            mCineIoClient.broadcast(stream.getId(), mBroadcastConfig, mContext);
+            this.setStreamingState(true);
+        }
+    }
+
+    public void destroyCurrentStream() {
+
+        if (!this.isStreaming()) {
+            return;
+        }
+        // Makes sure there is an actual stream.
+        if (currentStream != null) {
+            new AsyncTask<Void, Void, Stream>() {
+                protected Stream doInBackground(Void... params) {
+                    try {
+                        String id = currentStream.getId();
+                        String password = currentStream.getPassword();
+                        Log.i(TAG, "DELETING " + id + "?p=" + password);
+
+                        return streamsApi.streamStreamIdDelete(id, password, password);
+
+                    } catch (ApiException e) {
+                        showAPIError("" + e.getCode(), e.getMessage());
+                        // e.printStackTrace();
+                    }
+                    return null;
+                }
+
+                @Override
+                public void onPostExecute(Stream result) {
+                    Log.i(TAG, "Stream deleted (" + result.getId() + ")");
+                    setStreamingState(false);
+                }
+            }.execute();
+        }
+    }
+
+    private void showAPIError(String code, String message) {
+
+
+        // TODO: Display a message to the user
+        Log.e(TAG, "Code: " + code);
+        Log.e(TAG, "Message: " + message);
+
+//        AlertDialog.Builder builder = new AlertDialog.Builder(this.mContext);
+//        builder.setMessage(message);
+//
+//        // 3. Get the AlertDialog from create()
+//        AlertDialog dialog = builder.create();
+//        dialog.show();
+
     }
 
     /**
