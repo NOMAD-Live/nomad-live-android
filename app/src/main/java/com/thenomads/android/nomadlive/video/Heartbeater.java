ae95f06c67cdb776e22b85de1a03fcb14a0b185b
C-A de Salaberry <cadesalaberry@yahoo.com>
2015-06-28 20:15:50 -0400

    Mod: Standardise Heartbeater.

--- app/src/main/java/com/thenomads/android/nomadlive/video/Heartbeater.java
+++ app/src/main/java/com/thenomads/android/nomadlive/video/Heartbeater.java
@@ -1,5 +1,6 @@
 package com.thenomads.android.nomadlive.video;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.util.Log;
@@ -12,29 +13,31 @@ import io.swagger.client.api.HeartbeatApi;
 import io.swagger.client.model.Stream;
 
 /**
- * Maintains the stream alive sending a heartbeat every 5s.
+ * Maintains the mStream alive sending a heartbeat every 5s.
  */
 public class Heartbeater {
 
     private final String TAG = "Heartbeater";
-
     private final int INTERVAL = 5000; // In ms
-    private HeartbeatApi api;
-    private Timer timer;
+
+    private final Context mContext;
+    private HeartbeatApi mHeartbeatAPI;
+    private Timer mTimer;
 
     // Stream to keep alive
-    private Stream stream;
+    private Stream mStream;
 
-    public Heartbeater(Stream stream) {
-        this.api = new HeartbeatApi();
-        this.timer = new Timer();
+    public Heartbeater(Stream stream, Context context) {
+        this.mHeartbeatAPI = new HeartbeatApi();
+        this.mTimer = new Timer();
 
-        this.stream = stream;
+        this.mContext = context;
+        this.mStream = stream;
     }
 
     public void start() {
 
-        Log.v(TAG, "Keeping alive stream: " + stream.getId());
+        Log.v(TAG, "Keeping alive stream: " + mStream.getId());
 
         final Handler handler = new Handler();
 
@@ -45,54 +48,48 @@ public class Heartbeater {
                     public void run() {
                         try {
 
-                            Log.v(TAG, "Beating for: " + stream.getId());
-                            oneBeat().execute(stream);
+                            Log.v(TAG, "Beating for: " + mStream.getId());
+                            oneBeat().execute(mStream);
                         } catch (Exception e) {
-                            Log.e(TAG, "Could not execute one beat for " + stream);
+                            Log.e(TAG, "Could not execute one beat for " + mStream);
                             e.printStackTrace();
                         }
                     }
                 });
             }
         };
-        timer.schedule(doAsynchronousTask, 0, INTERVAL); //execute in every 50000 ms
+        mTimer.schedule(doAsynchronousTask, 0, INTERVAL); //execute in every 50000 ms
     }
 
     public void stop() {
-        Log.v(TAG, "Letting die stream: " + stream.getId());
-        timer.cancel();  // Terminates this timer, discarding any currently scheduled tasks.
-        timer.purge();
+        Log.v(TAG, "Letting die stream: " + mStream.getId());
+        mTimer.cancel();  // Terminates this timer, discarding any currently scheduled tasks.
+        mTimer.purge();
     }
 
-    private AsyncTask<Stream, Void, Void> oneBeat() {
-        return new AsyncTask<Stream, Void, Void>() {
+    private AsyncTask<Stream, Void, ApiException> oneBeat() {
+        return new AsyncTask<Stream, Void, ApiException>() {
 
             @Override
-            protected Void doInBackground(Stream... params) {
-                String id = stream.getId();
-                String password = stream.getPassword();
+            protected ApiException doInBackground(Stream... params) {
+                String id = mStream.getId();
+                String password = mStream.getPassword();
                 try {
-                    api.streamStreamIdPost(id, password, password);
+                    mHeartbeatAPI.streamStreamIdPost(id, password, password);
                 } catch (ApiException e) {
-                    showAPIError(e);
+                    return e;
                 }
                 return null;
             }
+
+            @Override
+            protected void onPostExecute(ApiException result) {
+                super.onPostExecute(result);
+
+                if (result != null) {
+                    VideoBroadcaster.showAPIErrorIfAny(result, mContext);
+                }
+            }
         };
-    }
-
-    private void showAPIError(ApiException e) {
-
-        // TODO: Display a message to the user
-        Log.e(TAG, "Code: " + e.getCode());
-        Log.e(TAG, "Message: " + e.getMessage());
-
-//        AlertDialog.Builder builder = new AlertDialog.Builder(this.mContext);
-//        builder.setMessage(message);
-//
-//        // 3. Get the AlertDialog from create()
-//        AlertDialog dialog = builder.create();
-//        dialog.show();
-
     }
 }
